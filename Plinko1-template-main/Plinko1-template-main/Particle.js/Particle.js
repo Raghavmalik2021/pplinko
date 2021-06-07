fb0c2d4d7f4208455810d06f5c5e4b27997f7893
Raghavmalik2021 <85512295+Raghavmalik2021@users.noreply.github.com>
2021-06-07 23:00:30 +0530

    Add files via upload

---
+++ Plinko1-template-main/Plinko1-template-main/Particle.js/Particle.js
@@ -0,0 +1,13 @@
+Particle.class()
+
+
+if(frameCount%60===0 ){
+
+    particles.push(new Particle(random(width/2-10,width/2-10),10,10));
+}
+display()
+this.body = bodies.circle(x,y,this.releaseEvents,options);
+this.color= (random(0,255),random(0,255),random(0,255));
+world.add(world.this.body)
+
+  
